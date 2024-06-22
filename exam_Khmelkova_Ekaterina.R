d8a3c4a84fc38f4b80c2559ddc037978b5eb0de1
Your Name <you@example.com>
2024-06-22 19:04:35 +0300

    11

--- exam_Khmelkova_Ekaterina.R
+++ exam_Khmelkova_Ekaterina.R
@@ -1,28 +1,40 @@
 # question 1
-library(ggplot)
-mpg
-help(mpg)
-#ggg
-#hhh
+
+d = data(mtcars)
+mtcars
+t.test(mtcars)
+
+
 # question 2
-
+#python
 # question 3
-
+#python
 # question 4
-
+#python
 # question 5
-
+data(mtcars)
+mtcars
+mtcars_aov <- aov(mtcars$disp~factor(mtcars$carb))
+summary(mtcars_aov)
 # question 6
-
+#python
 # question 7
-
+#python
 # question 8
 
 # question 9
+d = data(mtcars)
+mtcars
+mtcars$cyl == 6
+mean(mtcars$mpg)
 
 # question 10
 
 # question 11
+
+groupA <- c(150, 200, 250)
+groupB <- c(150, 200, 250)
+t.test(groupA, groupB)
 
 # question 12
 
@@ -33,6 +45,12 @@ help(mpg)
 # question 15
 
 # question 16
+d = data(mtcars)
+mtcars
+model = lm(mtcars$mpg ~ mtcars$hp + mtcars$drat + mtcars$disp)
+g = data.frame(hp = 110, drat = 3.9, disp = 200)
+predict(model, g)
+
 
 # question 17
 
